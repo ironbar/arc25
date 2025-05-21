e4aeed80bc14547b18a56328ae608bf8c53fc151
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2025-05-21 15:33:51 +0200

    Fix environment directory naming and improve pip installation command in run_condor_job.sh

--- scripts/calculon/run_condor_job.sh
+++ scripts/calculon/run_condor_job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 # Exit immediately if a command exits with a non-zero status.
 set -e 
-# Arguments
+set -x
+#Arguments
 REQUIREMENTS_FILE=$1  # Path to requirements.txt
 # Shift the first argument to get the remaining arguments as $@
 shift 1
@@ -10,7 +11,7 @@ COMMAND=$@
 # Directories
 ENV_CACHE_DIR="/mnt/scratch/users/gbarbadillo/arc25/cached-environments"  # Directory to cache environments
 ENV_HASH=$(md5sum $REQUIREMENTS_FILE | awk '{print $1}')  # Hash the requirements.txt
-ENV_DIR="$ENV_CACHE_DIR/env_$ENV_HASH"
+ENV_DIR="$ENV_CACHE_DIR/venv_$ENV_HASH"
 
 # Check if the environment exists
 if [ -d "$ENV_DIR" ]; then
@@ -18,12 +19,11 @@ if [ -d "$ENV_DIR" ]; then
     source $ENV_DIR/bin/activate
 else
     echo "Environment does not exist. Creating a new one..."
-    unset PIP_USER
-    python3 -m venv $ENV_DIR
+    python3 -m venv $ENV_DIR 
     source $ENV_DIR/bin/activate
     pip3 install --upgrade pip
     pip3 install -r $REQUIREMENTS_FILE
-    pip3 install flash-attn==2.6.3 --no-build-isolation
+    MAX_JOBS=2 pip3 install flash-attn==2.6.3 --no-build-isolation
     echo "Environment created and cached at $ENV_DIR."
 fi
 

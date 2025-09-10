a642017b513ac5e218a6cc775c9df7f38d563623
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2025-09-10 04:45:15 +0200

    updated resource monitor to show moving mean

--- arc25/resource_monitor.py
+++ arc25/resource_monitor.py
@@ -1,5 +1,6 @@
 import psutil
 import GPUtil
+import numpy as np
 import matplotlib.pyplot as plt
 from time import time, sleep
 from threading import Thread
@@ -52,18 +53,18 @@ class ResourceMonitor:
         plt.figure(figsize=(14, 8))
         # Plot CPU usage
         plt.subplot(2, 1, 1)
-        plt.plot(self.timestamps, self.cpu_usage, label="CPU Usage (%)")
+        _plot_with_moving_mean(self.timestamps, self.cpu_usage)
         plt.xlabel("Time (s)")
         plt.ylabel("CPU Usage (%)")
-        plt.title("CPU Usage Over Time")
+        plt.title("CPU Usage Over Time. Mean: {:.1f}%".format(sum(self.cpu_usage)/len(self.cpu_usage)))
         plt.grid(True)
 
         # Plot RAM usage
         plt.subplot(2, 1, 2)
-        plt.plot(self.timestamps, self.ram_usage, label="RAM Usage (%)", color='orange')
+        _plot_with_moving_mean(self.timestamps, self.ram_usage, color='orange')
         plt.xlabel("Time (s)")
         plt.ylabel("RAM Usage (%)")
-        plt.title("RAM Usage Over Time")
+        plt.title("RAM Usage Over Time. Mean: {:.1f}%".format(sum(self.ram_usage)/len(self.ram_usage)))
         plt.grid(True)
         plt.tight_layout()
         plt.show()
@@ -73,10 +74,23 @@ class ResourceMonitor:
         n_gpus = len(self.gpu_usage)
         for gpu_id, usage in self.gpu_usage.items():
             plt.subplot(n_gpus, 1, gpu_id + 1)
-            plt.plot(self.timestamps, usage, label=f"GPU {gpu_id} Usage (%)")
+            _plot_with_moving_mean(self.timestamps, usage, color='green')
             plt.xlabel("Time (s)")
             plt.ylabel(f"GPU {gpu_id} Usage (%)")
-            plt.title(f"GPU {gpu_id} Usage Over Time")
+            plt.title(f"GPU {gpu_id} Usage Over Time. Mean: {sum(usage)/len(usage):.1f}%")
             plt.grid(True)
         plt.tight_layout()
         plt.show()
+
+
+def _plot_with_moving_mean(x, y, color, window_size=60):
+    plt.plot(x, y, color=color, alpha=0.3)
+    y_smooth = _moving_mean(np.array(y), window_size)
+    plt.plot(x, y_smooth, color=color, linestyle='--')
+
+
+def _moving_mean(a, window_size, mode="reflect"):
+    # pad with edge values
+    pad = window_size // 2
+    a_padded = np.pad(a, pad_width=pad, mode=mode)
+    return np.convolve(a_padded, np.ones(window_size), 'valid') / window_size

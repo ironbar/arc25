7456f4740cf245df530460402cae4cba047401a7
Guillermo Barbadillo <guillermobarbadillo@gmail.com>
2025-09-14 09:23:20 +0200

    added debug_parallel_execution script

---
+++ scripts/debug_parallel_execution.py
@@ -0,0 +1,63 @@
+from dataclasses import dataclass
+import tyro
+import glob
+from tqdm import tqdm
+
+from arc25.parallel_code_execution import run_code_from_predictions
+from arc25.metrics import aggregate_metrics, error_analysis
+from arc25.utils import load_arc_dataset_with_solutions, load_json
+
+@dataclass
+class Config:
+    dataset_path: str = "/mnt/hdd0/Kaggle/arc25/data/arc-prize-2024/arc-agi_evaluation_challenges.json"
+    prediction_path: str = "/mnt/hdd0/Kaggle/arc25/predictions/2025-08-28-base-model/evaluation/8preds_2025_08_31_09_47_48_predictions.json"
+    n_jobs: int = -1
+    timeout_duration: int = 1
+    batch_size: int = 5000
+
+
+def main(args=None):
+    config = tyro.cli(Config)
+    dataset = load_arc_dataset_with_solutions(config.dataset_path)
+
+    predictions = load_all_predictions(config.prediction_path)
+    n_preds = len(list(predictions.values())[0]['text_predictions'])
+    print(f"Loaded {len(predictions)} tasks with {n_preds} predictions each.")
+
+    tasks, task_ids, text_predictions, data_augmentation_params = [], [], [], []
+    for task_id, task_preds in predictions.items():
+        tasks.extend([dataset[task_id]] * len(task_preds['text_predictions']))
+        task_ids.extend([task_id] * len(task_preds['text_predictions']))
+        text_predictions.extend(task_preds['text_predictions'])
+        data_augmentation_params.extend(task_preds['data_augmentation_params'])
+
+    results = run_code_from_predictions(
+        tasks, task_ids, text_predictions, data_augmentation_params,
+        n_jobs=config.n_jobs, timeout_duration=config.timeout_duration, batch_size=config.batch_size)
+    df = aggregate_metrics(results)
+    error_analysis(results)
+    print(df.iloc[-1:])
+
+
+def load_all_predictions(path_pattern):
+    filepaths = glob.glob(path_pattern)
+    predictions = dict()
+    for filepath in tqdm(filepaths, desc="Loading predictions", disable=len(filepaths)<=1):
+        preds = load_json(filepath)
+        for task_id, outputs in preds.items():
+            if task_id not in predictions:
+                predictions[task_id] = dict(text_predictions=[], data_augmentation_params=[])
+            if isinstance(outputs, dict):
+                predictions[task_id]['text_predictions'].extend(outputs['text_predictions'])
+                data_augmentation_params = outputs.get('data_augmentation_params', None)
+                if data_augmentation_params is not None and data_augmentation_params['color_map'] is not None:
+                    data_augmentation_params['color_map'] = {int(k): int(v) for k, v in data_augmentation_params['color_map'].items()}
+                predictions[task_id]['data_augmentation_params'].extend([data_augmentation_params]*len(outputs['text_predictions']))
+            else:
+                predictions[task_id]['text_predictions'].extend(outputs)
+                predictions[task_id]['data_augmentation_params'].extend([None] * len(outputs))  # Assuming no params for old format
+    return predictions
+
+
+if __name__ == '__main__':
+    main()
